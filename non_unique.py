060a4d7c249e4cbbdfa4068fc6be52bb6d5b76d8
Chris Moser <chrismmoser@gmail.com>
2016-03-08 12:42:16 -0600

    did not catch 1 lower and one upper str

--- non_unique.py
+++ non_unique.py
@@ -4,7 +4,7 @@ def non_unique(data):
     output = []
     for i in data:
         if isinstance(i, str):
-            if data.count(i.upper()) > 1 or data.count(i.lower()) > 1:
+            if data.count(i.upper()) + data.count(i.lower()) > 1:
                 output.append(i)
         else:
             if data.count(i) > 1:
@@ -14,4 +14,4 @@ def non_unique(data):
 
 non_unique([1,2,3,4,1,2])
 non_unique([1,2,'A',1,2,'A','a'])
-non_unique([1,'c','A',1,2,'A','a'])
+non_unique(['X','H','e','V','m','l','s',1,0,'y','j','b','g','E'])
